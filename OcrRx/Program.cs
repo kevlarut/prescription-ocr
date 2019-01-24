21279b48d29f81ef7c68bfdb07077d702919191a
Kevin L. Owens <kowens@gmail.com>
2019-01-23 19:38:08 -0700

    Add files via upload
    Basic implementation of Tesseract to run OCR on the prescription image.

---
+++ OcrRx/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Tesseract;
+
+namespace OcrRx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImagePath = "./rx.png";
+            if (args.Length > 0)
+            {
+                testImagePath = args[0];
+            }
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"./", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+                            Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            Console.WriteLine("Text (iterator):");
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+                                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                {
+                                                    Console.WriteLine("<BLOCK>");
+                                                }
+
+                                                Console.Write(iter.GetText(PageIteratorLevel.Word));
+                                                Console.Write(" ");
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    Console.WriteLine();
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                Console.WriteLine();
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
